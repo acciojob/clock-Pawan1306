e917244ea9c8ab3610d5be3bcc774eeac8628eeb
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-11-02 22:55:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function updateTimer() {
+    const timerElement = document.getElementById("timer");
+    const now = new Date();
+    const dateStr = now.toDateString();
+    const timeStr = now.toLocaleTimeString();
+
+    timerElement.textContent = `${dateStr} ${timeStr}`;
+}
+
+// Update the timer initially
+updateTimer();
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
+
